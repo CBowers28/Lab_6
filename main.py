175c5f6f21b02054dbabd0f2d9a4f7d38a45d5b3
CBowers28 <cj.bowers@ufl.edu>
2023-10-25 12:04:58 -0400

    Print menu function and encode logic

---
+++ main.py
@@ -0,0 +1,36 @@
+#function is used to print menu and return user input
+def print_menu():
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    user_choice =int(input("\nPlease enter an option: "))
+    return user_choice
+
+def main():
+    condition = True
+    #While loop runs the program and user choice equals return value from print_menu function
+    while condition == True:
+        user_choice = print_menu()
+        if user_choice == 1:
+            #Takes in decoded password and itrates through it adding 3 to each value and then storing it as encoded_password
+            decoded_password = input("Please enter your password to encode: ")
+            encoded_password = ""
+            for char in decoded_password:
+                encoded_password_character = int(char)+3
+                encoded_password += str(encoded_password_character)
+            print("Your password has been encoded and stored!")
+
+        if user_choice == 2:
+            print("hi")
+
+        if user_choice == 3:
+            break
+
+
+
+
+if __name__ == "__main__":
+    main()
+
